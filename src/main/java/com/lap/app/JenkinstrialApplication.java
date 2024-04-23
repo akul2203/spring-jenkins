ce1ffee6f8f5ef33b00b0dd14bf2890964ba647e
akul2203 <gehlotakul@gmail.com>
2024-04-23 17:15:30 +0530

    pipeline

--- src/main/java/com/lap/app/JenkinstrialApplication.java
+++ src/main/java/com/lap/app/JenkinstrialApplication.java
@@ -1,15 +1,23 @@
 package com.lap.app;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger; // Import Logger class
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinstrialApplication {
 
-	public static void main(String[] args) {
-		System.err.println("Jenkis test ::::::::::::::::::::::::::::::::::{{Test Success}}");
-		System.out.println("ohk its done or fake jenkins");
-		SpringApplication.run(JenkinstrialApplication.class, args);
-	}
+    // Declare logger instance
+    private static final Logger logger = LoggerFactory.getLogger(JenkinstrialApplication.class);
 
+    public static void main(String[] args) {
+        System.err.println("Jenkins test ::::::::::::::::::::::::::::::::::{{Test Success}}");
+        logger.info("Jenkins test ::::::::::::::::::::::::::::::::::{{Test Success}}");
+        // Log a message at INFO level
+        logger.info("Starting JenkinstrialApplication...");
+        logger.info("test Log -------------------------------------Starting JenkinstrialApplication...");
+
+        SpringApplication.run(JenkinstrialApplication.class, args);
+    }
 }
